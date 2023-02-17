99654b2081ea4ff19d9ec789b8945138302216b4
heavy-matill <heavymatill@gmail.com>
2023-02-17 14:23:04 +0100

    manual publish and receive working

--- background.js
+++ background.js
@@ -14,14 +14,13 @@ async function initialize() {
     userId = `${Date.now()}-${Math.floor(Math.random() * 100)}`;
     chrome.storage.local.set({ "userId": userId });
   }
-  session = "session"
-  /*if (storageCache["session"]) {
+  if (storageCache["session"]) {
     session = storageCache["session"];
   }
   else {
     session = "session";
     chrome.storage.local.set({ "session": session });
-  }*/
+  }
   console.log(userId)
   client = mqtt.connect('wss://broker.emqx.io:8084/mqtt', { clientId: userId })
 
@@ -31,68 +30,20 @@ async function initialize() {
         client.publish('presence', 'Hello mqtt')
       }
     })
-    client.subscribe(session, function (err) {
-      if (!err) {
-        client.publish(session, 'new user')
-      }
-    })
+    client.subscribe(session)
   })
 
   client.on('message', function (topic, message) {
-    // message is Buffer
-    console.log(message.toString())
+    console.log(topic.toString(), message.toString())
+    if (topic === session) {
+      let sessionSongUrl = message.toString().replace("sessionSong:", "");
+      if (sessionSongUrl)
+        chrome.storage.local.set({ "sessionSong": sessionSongUrl })
+    }
     //client.end()
   })
 
-  /* Paho
-//script.onload = function () {
-  //do stuff with the script
-  // Create a client instance
-  console.log(userId)
-  client = new Paho.MQTT.Client("wss://broker.emqx.io:8084/mqtt", userId);
-  // always reconnect
-  client.loop_forever();
-
-
-  // set callback handlers
-  client.onConnectionLost = onConnectionLost;
-  client.onMessageArrived = onMessageArrived;
-
-  // connect the client
-  client.connect({ onSuccess: onConnect });
-
-
-  // called when the client connects
-  function onConnect() {
-    // Once a connection has been made, make a subscription and send a message.
-    console.log("onConnect");
-    client.subscribe("World");
-    message = new Paho.MQTT.Message("Hello");
-    message.destinationName = "World";
-    client.send(message);
-    if (session) {
-      client.subscribe(session);
-    }
-  }
-
-  // called when the client loses its connection
-  function onConnectionLost(responseObject) {
-    if (responseObject.errorCode !== 0) {
-      console.log("onConnectionLost:" + responseObject.errorMessage);
-    }
-  }
-
-  // called when a message arrives
-  function onMessageArrived(message) {
-    console.log("onMessageArrived:" + message.payloadString);
-  }*/
-
-
 }
-//};
-//script.src = "https://cdnjs.cloudflare.com/ajax/libs/paho-mqtt/1.0.1/mqttws31.min.js";
-
-//document.head.appendChild(script); //or something of the likes
 
 var storageCache = {};
 chrome.storage.local.get(null, function (data) {
@@ -101,34 +52,28 @@ chrome.storage.local.get(null, function (data) {
 });
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
   if (changeInfo.status === 'complete' && tab.url && tab.url.includes(baseSongUrl)) {
-    const songUrl = getSongUrl(tab.url);
-    console.log("newSong", songUrl)
-
-    /*chrome.tabs.sendMessage(tabId, {
-      type: "newSong",
-      songUrl: songUrl,
-    });*/
     chrome.scripting.executeScript({
       target: { tabId: tabId },
-      files: [ "contentScript.js"]
+      files: ["utils2.js", "contentScript.js"]
     })
-    console.log("executed contetnScript")
   }
 });
 
 
 chrome.runtime.onMessage.addListener((obj, sender, response) => {
-  const { type, value, payload } = obj;
-
-  if (type === "joinSession") {
-    // redundant, solved via storage
+  const { message, payload } = obj;
+  console.log(obj)
+  if (message === "publishSong") {
+    console.log("Publishing via mqtt:", session, payload)
+    client.publish(session, 'sessionSong:' + payload)
   }
 });
 function subscribeNewSession(oldSession, newSession) {
   console.log("subscribing new session")
   if (oldSession)
-    client.unsubscribe(oldSession);
-  client.subscribe(newSession);
+    client.unsubscribe(oldSession)
+  chrome.storage.local.set({ sessionSong: "" })
+  client.subscribe(newSession)
 }
 chrome.storage.onChanged.addListener((changes, namespace) => {
   for (let [key, { oldValue, newValue }] of Object.entries(changes)) {
--- contentScript.js
+++ contentScript.js
@@ -4,20 +4,29 @@ console.log("inside")
 
 let pub = true;
 let sub = true;
+var autoGet = false;
 var html = `
 <div style="height:1000px;width:500px;">
 <div id="floatbar" tabindex="1">
-  <span class="material-symbols-outlined">
+  <span id="receive-icon" class="material-symbols-outlined receive-icon">
     download
   </span>
+
+  <div class="float-button" >
+  <span class="sync-settings hide-sync-settings">
+  <input class="sync-settings hide-sync-settings" type="checkbox"> automatically get
   
   <span class="float-icon material-symbols-outlined">
     download
     <span class="float-icon-badge material-symbols-outlined">
     sync
+    </span>
   </span>
+</div>
+  
+  
   </span>
-  <span class="material-symbols-outlined">
+  <span id="sync-publish" class="material-symbols-outlined">
     publish
   </span>
   <span class="material-symbols-outlined">
@@ -30,7 +39,7 @@ var html = `
     sync_problem
   </span>
   
-<a class="float-button" >
+<div class="float-button" >
 
     <input id="session-input" class="sync-settings hide-sync-settings">
   </input>
@@ -44,79 +53,99 @@ var html = `
 document.body.insertAdjacentHTML("beforeend", html);
 
 function toggleSettingsVisibility() {
-    console.log("toggle")
-    for (const x of document.getElementsByClassName('sync-settings')) { x.classList.toggle('hide-sync-settings') }
+  console.log("toggle")
+  for (const x of document.getElementsByClassName('sync-settings')) { x.classList.toggle('hide-sync-settings') }
 }
 document.getElementById("sync-settings-icon").addEventListener('click', toggleSettingsVisibility);
 
 function blurSettings(event) {
-    console.log("blur",event)
-    if (!(event.relatedTarget && (event.relatedTarget.classList.contains("sync-settings") || event.relatedTarget.id == "floatbar")))
-        toggleSettingsVisibility();
+  console.log("blur", event)
+  if (!(event.relatedTarget && (event.relatedTarget.classList.contains("sync-settings") || event.relatedTarget.id == "floatbar")))
+    toggleSettingsVisibility();
 }
 document.getElementById("floatbar").addEventListener('focusout', blurSettings);
 
+document.getElementById("sync-publish").addEventListener('click', function () { let actualSongUrl = getSongUrl(document.location.href); console.log("publishing.", actualSongUrl); publishSongUrl() });
+
 function blurSessionInput(event) {
-    console.log(event.target.value)
-    chrome.storage.local.set({session: event.target.value})
+  console.log(event.target.value)
+  chrome.storage.local.set({ session: event.target.value })
 }
 document.getElementById("session-input").addEventListener('blur', blurSessionInput, true);
 
 /* storage initial check */
 chrome.storage.local.get(null, function (data) {
-    document.getElementById("session-input").value = data["session"]
-  });
-/*storage listeners*/
+  document.getElementById("session-input").value = data["session"]
+  document.getElementById("sync-settings-auto-get").value = data["autoGet"]
+  autoPush = false
+  let actualSongUrl = getSongUrl(document.location.href);
+  if (autoPush && (data["sessionSong"] === actualSongUrl)) {
+    publishSongUrl()
+  }
+});
+/* storage listeners */
 chrome.storage.onChanged.addListener((changes, namespace) => {
-    for (let [key, { oldValue, newValue }] of Object.entries(changes)) {
-      console.log(
-        `Storage key "${key}" in namespace "${namespace}" changed.`,
-        `Old value was "${oldValue}", new value is "${newValue}".`
-      );
-      if (key === "session") {
-        document.getElementById("session-input").value = newValue;
+  for (let [key, { oldValue, newValue }] of Object.entries(changes)) {
+    console.log(
+      `Storage key "${key}" in namespace "${namespace}" changed.`,
+      `Old value was "${oldValue}", new value is "${newValue}".`
+    );
+    if (key === "session") {
+      document.getElementById("session-input").value = newValue;
+    }
+    if (key === "sessionSong") {
+      if (newValue) {
+        if (newValue != getSongUrl(document.location.href)) {
+          if (autoGet)
+            receiveSong(newValue)
+          else {
+            receivedSongAvailable(newValue);
+          }
+        } else {
+          receivedSongIsOurs();
+        }
       }
     }
-  });
-/* other part */
-chrome.runtime.onMessage.addListener((obj, sender, response) => {
-    const { type, value, songUrl } = obj;
-
-    if (type === "newSong") {
-        if (pub && session) {
-            // we want to publish and we are part of a session
-            if (currentSongUrl != songUrl) {
-                // new songUrl is not equal to the latest subscribed change, othwise we may just have gotten in sync with the latest sub
-                publishSongUrl(songUrl);
-            }
-        }
-    } else if (type === "updatedSong") {
-        if (session) {
-            // we subscribed to a session
-            let activeSongUrl = getSongUrl(document.location.href);
-            let tempSongUrl = currentSongUrl;
-            currentSongUrl = songUrl;
-            if (activeSongUrl === tempSongUrl) {
-                // we were on the last active song when a new one was published
-                // update without asking
-                window.location = baseSongUrlHttps + songUrl;
-            } else {
-                // we were on a different song or browsing while a new one was published
-                // ask if user wants update
-            }
-
-        }
-    }
-    //var fixedDiv = document.createElement("div");
-    //fixedDiv.classList.add('fixed-action-btn horizontal click-to-toggle spin-close');
-    // floating actions from https://codepen.io/deseanwu/pen/gwNjve
-
+  }
 });
 
-const publishSongUrl = (songUrl) => {
-    // publish mqtt via background
+function receiveSong(songUrl) {
+  window.location = baseSongUrlHttps + songUrl;
+}
 
-    // set currentSongUrl
-    currentSongUrl = songUrl;
+function receivedSongAvailable(songUrl) {
+  console.log("receivedSongAvailable", songUrl)
+  let icon = document.getElementById('receive-icon');
+  icon.classList.add('receive-icon-available');
+  icon.addEventListener('click', function () {
+    receiveSong(songUrl);
+  });
+}
+
+function receivedSongIsOurs() {
+  let icon = document.getElementById('receive-icon');
+  icon.classList.remove('receive-icon-available');
+  icon.removeAttr('onclick');
+}
+
+/* message listeners */
+chrome.runtime.onMessage.addListener((obj, sender, response) => {
+  const { type, songUrl } = obj;
+  if (type === "anything?") {
+  }
+});
+
+function publishSongUrl() {
+  // send message to background worker
+  let actualSongUrl = getSongUrl(document.location.href);
+  console.log("publishSongUrl", actualSongUrl)
+  chrome.runtime.sendMessage({
+    message: "publishSong",
+    payload: actualSongUrl
+  }/*, response => {
+    if (response.message === 'success') {
+      ce_name.innerHTML = `Hello ${ce_input.value}`;
+    }
+  }*/);
 }
 
